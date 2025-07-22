b2679652634f7a62317e8c6894c219d7d1beda8d
rizkyaziz <rizkyaziz.hsn@gmail.com>
2025-07-22 22:28:26 +0700

    feat: add implement authentication - Add login page and form with validation using Zod and React Hook Form - Add UI components: Button, Card, Label, Form

---
+++ src/app/(auth)/login/_components/Login.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { INITIAL_LOGIN_FORM } from "@/constants/auth-constant";
+import { LoginForm, loginSchema } from "@/validations/auth-validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const Login = () => {
+  const form = useForm<LoginForm>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: INITIAL_LOGIN_FORM,
+  });
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    console.log(data);
+  })
+  return (
+    <Card>
+      <CardHeader className="text-center">
+        <CardTitle className="text-xl">Welcome</CardTitle>
+        <CardDescription>
+          Please enter your credentials to log in.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={onSubmit} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field: { ...rest } }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...rest}
+                      type="email"
+                      placeholder="Enter your email"
+                      autoComplete="off"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field: { ...rest } }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...rest}
+                      type="password"
+                      placeholder="Enter your password"
+                      autoComplete="off"
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Login;
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './_components/Login'
+
+export const metadata = {
+  title: "Cafe | Login"
+}
+
+const LoginPage = () => {
+  return <Login />
+}
+
+export default LoginPage
